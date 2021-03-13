71a2a3caa7abc8bcccb4426f23f27e013270a5cf
Larry Hodges <74116412+imhotep-hodges@users.noreply.github.com>
2021-03-13 11:26:43 -0500

    ToDo List Template installed

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import '/Users/L-Hodges/my-app/src/index.css';
+
+import Todolist from './components/Todolist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a test
-        </p>
-        <div className = "jumbotron">
-          <h1>HNIC Prodigy</h1>
-          <p>QB Long Island City NY</p>
-        </div>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELP ME Learn React
-        </a>
-        
-      </header>
-      
+    <div className="todo-app">
+      <h1>ToDo App</h1>
+      <Todolist />
     </div>
   );
 }
